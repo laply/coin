eb28b48e49eb5d383636cf19ffde02c3b7218d15
laply_macbook Air <lapply2635@gmail.com>
2022-01-15 19:33:13 +0900

    [ADD] make block chain

--- main.go
+++ main.go
@@ -11,8 +11,35 @@ type block struct {
 	prevHash string
 }
 
+type blockChain struct {
+	blocks []block
+}
+func (bc *blockChain) makeHash(data string) string {
+	return fmt.Sprintf("%x",sha256.Sum256([]byte(data)))
+}
+
+func (bc *blockChain) getLastHash() string {
+	if(len(bc.blocks) != 0) {
+		return  bc.blocks[len(bc.blocks)-1].hash
+	}
+	return ""
+}
+
+func (bc *blockChain) addBlock(data string){
+	bc.blocks = append(bc.blocks, block{data, bc.makeHash(data),  bc.getLastHash()})
+}
+
+func (bc blockChain) listBlocks(){
+	for _, data := range bc.blocks {
+		fmt.Printf("Data: %s\nHash: %s\nPrev Hash: %s\n", data.data, data.hash, data.prevHash)
+	}
+}
+
+
 func main() {
-	genesisBlock := block{"genesis Block", "", ""}
-	genesisBlock.hash = fmt.Sprintf("%x",sha256.Sum256([]byte(genesisBlock.data+ genesisBlock.prevHash)))
-	fmt.Println(genesisBlock)
+	chain := blockChain{}
+	chain.addBlock("1st")
+	chain.addBlock("2nd")
+	chain.addBlock("3rd")
+	chain.listBlocks()
 }
