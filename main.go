3897718baeb2a98bce4ec58fefbc60607b3bd76d
laply_macbook Air <lapply2635@gmail.com>
2022-02-08 09:08:27 +0900

    [add] REST API 6.4

--- main.go
+++ main.go
@@ -1,8 +1,13 @@
 package main
 
-import "github.com/laply/coin/explorer"
-
+import (
+	"github.com/laply/coin/explorer"
+	"github.com/laply/coin/rest"
+)
 
 func main() {
-	explorer.Start()
+	go explorer.Start(4200)
+	rest.Start(4210)
 }
+
+
---
+++ utils/utils.go
@@ -0,0 +1,7 @@
+package utils
+
+import "log"
+
+func HandleErr(err error){
+	if err != nil { log.Panic(err)}
+}
