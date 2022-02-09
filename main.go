21ff888e48e777090dceb519bec6ef3688188170
laply_macbook Air <lapply2635@gmail.com>
2022-02-09 09:34:58 +0900

    [ADD] CLi base & Flagset

--- main.go
+++ main.go
@@ -1,13 +1,43 @@
 package main
 
 import (
-	"github.com/laply/coin/explorer"
+	"flag"
+	"fmt"
+	"os"
+
 	"github.com/laply/coin/rest"
 )
 
+func usage () {
+	fmt.Printf("Welcome to coin\n\n")
+	fmt.Printf("Please use the following command:\n\n")
+	fmt.Printf("explorer: start the HTML Explorer \n")
+	fmt.Printf("rest	: start the REST API (recommended) \n\n")
+	os.Exit(0)
+}
+
 func main() {
-	go explorer.Start(4200)
-	rest.Start(4210)
+	if len(os.Args) < 2 {
+		usage()
+	}
+
+	restFlags := flag.NewFlagSet("rest", flag.ExitOnError)
+	portFlag := restFlags.Int("port", 4210, "Set the port of the server")
+
+	switch os.Args[1]{
+		case "explorer", "Explorer":
+			fmt.Println("Start Explorer")
+		case "rest", "REST":
+			restFlags.Parse(os.Args[2:])
+		default:
+			usage()			
+	}
+
+	if restFlags.Parsed() {
+		rest.Start(*portFlag)
+		// fmt.Println("Start REST API")
+	}
+
 }
 
 
