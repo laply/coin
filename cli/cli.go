2b8056a2a88700b6e65decf4cf3c672dbae0b174
laply_macbook Air <lapply2635@gmail.com>
2022-02-09 10:00:56 +0900

    [ADD] CLI

---
+++ cli/cli.go
@@ -0,0 +1,41 @@
+package cli
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/laply/coin/explorer"
+	"github.com/laply/coin/rest"
+)
+
+func usage () {
+	fmt.Printf("Welcome to coin\n\n")
+	fmt.Printf("Can use the following flags:\n\n")
+	fmt.Printf("-port : 	Set the PORT of the server\n")
+	fmt.Printf("-mode : 	Choose between 'html' and 'rest'  \n\n")
+	// os.Exit(0)
+}
+
+func Start() {
+	if len(os.Args) == 1{
+		usage()
+	}
+
+
+	port := flag.Int("port", 4000, "Set port of the server")
+	mode := flag.String("mode", "rest", "Choose between 'html' and 'rest'")
+
+	flag.Parse()
+
+	switch *mode{
+		case "rest":
+			rest.Start(*port)
+		case "html":
+			explorer.Start(*port)
+		default:
+			usage()
+
+	}
+
+}
